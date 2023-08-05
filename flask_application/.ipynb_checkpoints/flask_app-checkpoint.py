568d7c7ccd4371dfbd9d7af24d2f25faab35ef34
Julien Claveau <julienclaveau07@gmail.com>
2023-08-05 09:58:46 +0200

    Organisation dossier suite

---
+++ flask_application/.ipynb_checkpoints/flask_app-checkpoint.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+import os
+import git
+
+app = Flask(__name__)
+
+# Get the absolute path of the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Load pre-trained ML model using the absolute path
+MODEL_PATH = os.path.join(current_dir, 'flask_application/model.joblib')
+model = joblib.load(MODEL_PATH)
+
+@app.route('/git_update', method=['POST'])
+def git_update():
+    repo = git.Repo(current_dir)
+    origin = repo.remotes.origin
+    repo.create_head('main',
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # get the data in JSON format and transform it in pd.DataFrame
+        data_json = request.get_json()
+        data = pd.DataFrame(data_json['dataframe_split']['data'], columns=data_json['dataframe_split']['columns'], index=data_json['dataframe_split']['index'])
+        
+        # Make predictions using the loaded model
+        predictions = model.predict(data)
+        
+        return jsonify({'prediction' : str(predictions[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run()
---
+++ streamlit_dashboard/.ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import os
+
+st.title('Dashboard - Scoring crédit')
+
+DATA_URL = 'Data/train.csv'
+MODEL_URL_MLFLOW = 'http://127.0.0.1:5001/invocations'
+MODEL_URL_FLASK = 'https://ocjulienclaveau.eu.pythonanywhere.com/predict'
+
+username = os.environ.get('PA_USERNAME')
+password = os.environ.get('PA_PASSWORD')
+
+@st.cache_data
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    st.write(data.shape)
+    return data
+
+def get_data_client(id_client):
+    data = load_data()
+    data_client = data.loc[data['SK_ID_CURR'] == id_client, :]
+    data_client = data_client.replace(np.nan, None)
+    st.write(data_client.shape)
+    return data_client
+
+def request_prediction(model_uri, data):
+    headers = {"Content-Type": "application/json"}
+
+    data_json = {'dataframe_split' : data.to_dict(orient='split')}
+    
+    response = requests.request(
+        method='POST', headers=headers, url=model_uri, json=data_json, auth=(username, password))
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
+
+                 
+def main():
+
+    id_client = st.number_input('Id Client', value=0)
+    data = get_data_client(id_client)
+    
+    st.dataframe(data)
+    
+    predict_btn = st.button('Prédire')
+
+    if predict_btn:
+        response = request_prediction(MODEL_URL_FLASK, data)
+        
+        st.write(response)
+
+        if int(response['prediction']) == '0' :
+            st.write('Accordé')
+        else :
+            st.write('refusé')
+      
+                 
+if __name__ == '__main__':
+    main()
