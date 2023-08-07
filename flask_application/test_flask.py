8c7793113e5126362c8ff2a15880ef129d8b4bff
Julien Claveau <julienclaveau07@gmail.com>
2023-08-07 12:29:29 +0200

    update test_flask.py

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -10,13 +10,13 @@ class Testflaskapp(unittest.TestCase):
     
     def SetUp(self):
         app.config['TESTING'] = True
-        self.app = app.test_client()
+        self.client = app.test_client()
         
     def test_request_predict(self):
         headers = {"Content-Type": "application/json"}
         data_test = pd.read_csv('data_test.csv')
         data_json = {'dataframe_split' : data_test.to_dict(orient='split')}
-        rep = self.app.post('/predict', headers=headers, json=data_json)
+        rep = self.client.post('/predict', headers=headers, json=data_json)
         
         self.assertEqual(rep.status_code, 200, 'Erreur lors de la requete : {}'.format(rep.status_code))
         self.assertIn(rep['prediction'], [0, 1], 'Incorrect model output !')
