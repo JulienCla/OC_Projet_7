2d86b6677d0456a5dcc2da3f2b28cc6db605b3f5
Julien Claveau <julienclaveau07@gmail.com>
2023-08-07 12:48:01 +0200

    update test_flask.py

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -17,9 +17,10 @@ class Testflaskapp(unittest.TestCase):
         data_test = pd.read_csv('data_test.csv')
         data_json = {'dataframe_split' : data_test.to_dict(orient='split')}
         rep = self.client.post('/predict', headers=headers, json=data_json)
+        rep_data = rep.get_json()
         
         self.assertEqual(rep.status_code, 200, 'Erreur lors de la requete : {}'.format(rep.status_code))
-        self.assertIn(rep['prediction'], [0, 1], 'Incorrect model output !')
+        self.assertIn(rep_data['prediction'], [0, 1], 'Incorrect model output !')
         
 if __name__ == '__main__':
     unittest.main()
