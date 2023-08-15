bec01549098a75a580e8ed8c24f3d920ee6187c3
Julien Claveau <julienclaveau07@gmail.com>
2023-08-15 09:56:35 +0200

    lime explainer

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -20,6 +20,7 @@ class Testflaskapp(unittest.TestCase):
         response_data = response.get_json()
         
         self.assertEqual(response.status_code, 200, 'Erreur lors de la requete : {}'.format(response.status_code))
+        print(response_data)
         self.assertIn(response_data['prediction'], [0, 1], 'Incorrect model output !')
         
 if __name__ == '__main__':
