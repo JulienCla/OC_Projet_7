afb9524b1f371eea73b127b0acd401041813fe33
Julien Claveau <julienclaveau07@gmail.com>
2023-08-17 13:57:45 +0200

    Ajout fct flask api pour charger explainer au debut

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -10,6 +10,7 @@ class Testflaskapp(unittest.TestCase):
     
     def setUp(self):
         app.config['TESTING'] = True
+        self.app_context = app.app_context()
         self.client = app.test_client()
         
     def test_request_predict(self):
