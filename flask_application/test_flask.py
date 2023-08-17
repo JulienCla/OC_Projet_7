970b20ea0ac267cba2dd94d7f0ef73bdcc9e5880
Julien Claveau <julienclaveau07@gmail.com>
2023-08-17 14:26:04 +0200

    Update test_flask.py

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -1,9 +1,9 @@
 import unittest
 import pandas as pd
 import sys
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 sys.modules['git'] = MagicMock()
-from flask_app import app
+from flask_app import app, get_or_create_explainer
 
 
 class Testflaskapp(unittest.TestCase):
@@ -13,8 +13,14 @@ class Testflaskapp(unittest.TestCase):
         self.app_context = app.app_context()
         self.app_context.push()
         self.client = app.test_client()
+    
+    @patch('app.get_or_create_explainer')
+    def test_request_predict(self, mock_get_or_create_explainer):
         
-    def test_request_predict(self):
+        mock_explainer = mock_get_or_create_explainer.return_value
+        mock_explainer.explain_instance.return_value = "Mock explanation"
+        
+        
         headers = {"Content-Type": "application/json"}
         data_test = pd.read_csv('data_test.csv')
         data_json = {'dataframe_split' : data_test.to_dict(orient='split')}
@@ -24,6 +30,7 @@ class Testflaskapp(unittest.TestCase):
         self.assertEqual(response.status_code, 200, 'Erreur lors de la requete : {}'.format(response.status_code))
         print(response_data)
         self.assertIn(response_data['prediction'], [0, 1], 'Incorrect model output !')
+        self.assertEqual(response_data['explanation'], "Mock explanation")
         
 if __name__ == '__main__':
     unittest.main()
