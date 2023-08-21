e2ace0f908bfb81f28feea2eaeec4c2a3ac6fe55
Julien Claveau <julienclaveau07@gmail.com>
2023-08-21 22:25:04 +0200

    update test_flask.py

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -1,6 +1,7 @@
 import unittest
 import pandas as pd
 import sys
+import joblib
 from unittest.mock import MagicMock
 sys.modules['git'] = MagicMock()
 from flask_app import app
@@ -13,7 +14,14 @@ class Testflaskapp(unittest.TestCase):
         self.app = app
         self.client = self.app.test_client()
         
-    
+    def test_loading_model(self):
+        data_test = pd.read_csv('data_test.csv')
+        data_test.drop('Unnamed: 0', axis=1, inplace=True)
+        
+        model = joblib.load('model.joblib')
+        pred = model.predict(data_test)
+        self.assertIn(pred, [0, 1], 'Incorrect model output !')
+        
     def test_request_predict(self):
         with self.app.app_context():   
             headers = {"Content-Type": "application/json"}
@@ -25,6 +33,7 @@ class Testflaskapp(unittest.TestCase):
 
             self.assertEqual(response.status_code, 200, 'Erreur lors de la requete : {}'.format(response.status_code))
             #self.assertIn(response_data['prediction'], [0, 1], 'Incorrect model output !')
-        
+            
+    
 if __name__ == '__main__':
     unittest.main()
