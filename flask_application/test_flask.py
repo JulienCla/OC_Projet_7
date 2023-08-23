f96085e619224066b8e1c1f99fd845a16a074b3f
Julien Claveau <julienclaveau07@gmail.com>
2023-08-23 11:51:23 +0200

    update streamlit_app.py

--- flask_application/test_flask.py
+++ flask_application/test_flask.py
@@ -6,6 +6,24 @@ from unittest.mock import MagicMock
 sys.modules['git'] = MagicMock()
 from flask_app import app
 
+# Création d'une fonction personnalisée pour effectuer des prédictions en fonction d'un seuil
+class CustomModelWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self, model, threshold=0.5):
+        self.model = model
+        self.threshold = threshold
+
+    def predict(self, context, model_input):
+        # Prédiction personnalisée avec le paramètre threshold
+        probabilities = self.model.predict_proba(model_input)
+        predictions = (probabilities[:, 1] >= self.threshold).astype(int)
+        return predictions
+    
+    def predict_proba(self, model_input, context=None):
+        return self.model.predict_proba(model_input)
+    
+    def model(self, context=None):
+        return self.model
+
 
 class Testflaskapp(unittest.TestCase):
     
