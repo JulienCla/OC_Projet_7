baa747125a32e8d0949a01b9b067fa032d1b50ad
Julien Claveau <julienclaveau07@gmail.com>
2023-08-23 12:26:39 +0200

    update streamlit_app.py

--- flask_application/flask_app.py
+++ flask_application/flask_app.py
@@ -20,23 +20,23 @@ model = joblib.load(MODEL_PATH)
 # Train lime explainer
 DATA_PATH = os.path.join(current_dir, 'lime_data.csv')
 
-# Création d'une fonction personnalisée pour effectuer des prédictions en fonction d'un seuil
-class CustomModelWrapper(mlflow.pyfunc.PythonModel):
-    def __init__(self, model, threshold=0.5):
-        self.model = model
-        self.threshold = threshold
+# # Création d'une fonction personnalisée pour effectuer des prédictions en fonction d'un seuil
+# class CustomModelWrapper(mlflow.pyfunc.PythonModel):
+#     def __init__(self, model, threshold=0.5):
+#         self.model = model
+#         self.threshold = threshold
 
-    def predict(self, context, model_input):
-        # Prédiction personnalisée avec le paramètre threshold
-        probabilities = self.model.predict_proba(model_input)
-        predictions = (probabilities[:, 1] >= self.threshold).astype(int)
-        return predictions
+#     def predict(self, context, model_input):
+#         # Prédiction personnalisée avec le paramètre threshold
+#         probabilities = self.model.predict_proba(model_input)
+#         predictions = (probabilities[:, 1] >= self.threshold).astype(int)
+#         return predictions
     
-    def predict_proba(self, model_input, context=None):
-        return self.model.predict_proba(model_input)
+#     def predict_proba(self, model_input, context=None):
+#         return self.model.predict_proba(model_input)
     
-    def model(self, context=None):
-        return self.model
+#     def model(self, context=None):
+#         return self.model
 
 # Function to create and return the explainer instance
 def get_explainer():
